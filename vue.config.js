001b26d912ed4311406c873047ce2612e45fea18
sunlifei1 <62050077+sunlifei1@users.noreply.github.com>
2021-06-18 17:44:33 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-	publicPath: '/',                  //根路径
-	outputDir: 'dist',
+	publicPath: './',
+  outputDir: "dist", // 输出文件目录
+  lintOnSave: false, // eslint 是否在保存时检查
+  assetsDir: 'static', // 配置js、css静态资源二级目录的位置  
 	devServer: {
 		open: true,              //自动开启首页
 		host: 'localhost',
