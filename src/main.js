40ac57d0e8d1f018f580b98642e629782c1d376c
sunlifei1 <62050077+sunlifei1@users.noreply.github.com>
2021-06-18 17:33:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ Vue.use(store)
 // axios
 import axios from 'axios'
 Vue.prototype.axios = axios;
-// axios.defaults.baseURL = "xxxxx";  //这里使用自己下载的网易云接口地址
-axios.defaults.baseURL = process.env.NODE_ENV == "production" ? "/" : "http://localhost:5000/";
+axios.defaults.baseURL = "https://netease-cloud-music-api-self-six.vercel.app/";  //这里使用自己下载的网易云接口地址
+//axios.defaults.baseURL = process.env.NODE_ENV == "production" ? "/" : "http://localhost:5000/";
 
 // 引入Vant
 import Vant from 'vant'
